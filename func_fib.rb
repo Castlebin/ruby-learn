5fbfb5a4f6a81510e0b687a563cee7285bb57da5
Castlebin <Castlebin@gmail.com>
2023-09-18 20:23:37 +0800

    简单写个 fibonacci 函数

---
+++ func_fib.rb
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+# 返回第 n 个 Fibonacci 数，假设 n 从 1 开始，第 1 个是 1，第 2 个是 1 ，依次类推
+def fib(n)
+    if (n < 0) 
+        raise "入参 n 必须 大于等于 1"
+    end
+    if n == 1 || n == 2
+        return 1
+    end
+
+    return fib(n - 1) + fib(n - 2)
+end
+
+puts fib(10)
+# puts fib(0)
